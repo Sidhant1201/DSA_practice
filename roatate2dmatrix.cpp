2c7d3c25eaef9d9b7f17f23f2e866a7b1e843cb4
sidhant yadav <sidhantyadav92@gmail.com>
2023-05-02 09:58:21 +0530

    changes

--- roatate2dmatrix.cpp
+++ roatate2dmatrix.cpp
@@ -5,7 +5,7 @@ private:
         for(int i =0; i< n; i++){
             for(int j =i+1;j< n;j++){
                 swap(matrix[i][j], matrix[j][i]);
-            }
+            }sd
         }
     }
     void rev(vector<vector<int>> &matrix){
