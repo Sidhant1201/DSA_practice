e5ee369783ff74b4b841680d6a3ff9c9565ffcd2
Sidhant <108919838+Sidhant1201@users.noreply.github.com>
2023-12-08 08:30:29 +0000

    implementing binary search tree

---
+++ implementation/binary_tree.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    Node *right;
+    Node *left;
+    int data;
+    Node(int d)
+    {
+        this->data = d;
+        this->right = NULL;
+        this->left = NULL;
+    }
+};
+
+int main()
+{
+    
+    cout << "Kings";
+}
