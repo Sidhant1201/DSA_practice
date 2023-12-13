6b582737cba4b23d31d8f22c26c7568d3181b417
Sidhant <108919838+Sidhant1201@users.noreply.github.com>
2023-12-13 06:33:35 +0000

    added traversal

--- implementation/binary_tree.cpp
+++ implementation/binary_tree.cpp
@@ -15,18 +15,60 @@ public:
     }
 };
 
-Node push_to_tree(int d)
+Node* push_to_tree(Node* root, int d)
 {
+    if(root == NULL){
+        root= new Node(d);
+        return root;
+    }
+    if(d> root->data){
+        root->right =push_to_tree(root->right, d);
+    }
+    else
+        root->left = push_to_tree(root->left, d);
+    
+    return root;
 }
 
 void create_tree(Node *&root)
 {
     int d;
+    cout<< "enter data:";
     cin >> d;
     while (d != -1)
     {
-        push_to_tree(d);
+        root = push_to_tree(root, d);
+        cout<< "enter data: ";
         cin >> d;
+    }
+}
+
+void traversal_by_level(Node *root){
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        Node* temp = q.front();
+        q.pop();
+        if(temp== NULL){
+            cout<< endl;
+            if(!q.empty()){
+                q.push(NULL);
+            }
+        }
+        else{
+            cout<< temp->data<<" ";
+
+            if(temp->left){
+                q.push(temp->left);
+    
+            }
+            if(temp->right){
+                q.push(temp->right);
+            }
+
+        }
+        
     }
 }
 
@@ -34,5 +76,5 @@ int main()
 {
     Node *root;
     create_tree(root);
-    cout << "Kings";
+    traversal_by_level(root);
 }
