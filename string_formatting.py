5dc7560d6bd8d6b6702a8e0704731836bad4c905
Sidhant-yadav-BC <sidhant.yadav@blenheimchalcot.com>
2024-06-04 16:49:24 +0530

    format for decimal places

--- string_formatting.py
+++ string_formatting.py
@@ -6,3 +6,8 @@ print("Hi Im {name}, and I am {age} years old".format(**person))
 for n in range(1, 11):
     sent = "The padding is {:03}".format(n)
     print(sent)
+    
+    
+pi = 3.14278
+
+print("pi is equal to {:.2f}".format(pi))
