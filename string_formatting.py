8753c25f6783008facb4fe1f42f94dd4646ce739
Sidhant-yadav-BC <sidhant.yadav@blenheimchalcot.com>
2024-06-04 16:48:09 +0530

    add padding to format

--- string_formatting.py
+++ string_formatting.py
@@ -1,3 +1,8 @@
 person = {'name': 'Omkar', 'age': 23}
 
 print("Hi Im {name}, and I am {age} years old".format(**person))
+
+
+for n in range(1, 11):
+    sent = "The padding is {:03}".format(n)
+    print(sent)
